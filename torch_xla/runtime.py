befa287aac02c8f8e6724b63cdc1ab0a3a11f957
James Xu <jameszianxu@tenstorrent.com>
2025-11-14 17:13:37 +0000

    Return bool so caller can optionally check if cache was cleared

--- torch_xla/runtime.py
+++ torch_xla/runtime.py
@@ -289,8 +289,12 @@ def get_num_cached_compilation_graph():
 
 
 def clear_computation_cache():
-  """Clears the XLA computation cache contents, if computation cache is initialized."""
+  """Clears the XLA computation cache contents, if computation cache is initialized.
+  Returns:
+    bool: whether the cache was cleared successfully.
+  """
   if not torch_xla._XLAC._xla_computation_cache_is_initialized():
     warnings.warn("Computation cache must be initialized to clear it.")
-    return
+    return False
   torch_xla._XLAC._xla_computation_cache_clear()
+  return True
